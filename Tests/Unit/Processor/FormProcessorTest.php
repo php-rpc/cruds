4162ffb9e38fe088bb91b8ae29458edc05a48ad7
Pavel Batanov <pavel@batanov.me>
2016-09-19 21:26:46 +0300

    form testing

--- Tests/Unit/Processor/FormProcessorTest.php
+++ Tests/Unit/Processor/FormProcessorTest.php
@@ -3,35 +3,44 @@
 namespace ScayTrase\Api\Cruds\Tests\Unit\Processor;
 
 use ScayTrase\Api\Cruds\Adaptors\Symfony\FormProcessor;
+use ScayTrase\Api\Cruds\Exception\EntityProcessingException;
+use ScayTrase\Api\Cruds\Tests\AbstractCrudsWebTest;
+use ScayTrase\Api\Cruds\Tests\Fixtures\SymfonySerializer\SymfonyTestKernel;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\Test\TypeTestCase;
 
-class FormProcessorTest extends TypeTestCase
+class FormProcessorTest extends AbstractCrudsWebTest
 {
     public function testProcessor()
     {
+        self::createAndBootKernel(SymfonyTestKernel::class);
+        $factory = static::$kernel->getContainer()->get('form.factory');
+
         $entity    = new \stdClass();
         $entity->b = null;
         $entity->a = null;
 
-        $processor = new FormProcessor($this->factory, TestForm::class);
-        $processor->updateEntity($entity, ['a' => 3, 'b' => 'a']);
+        $processor = new FormProcessor($factory, TestForm::class);
+        $processor->updateEntity($entity, ['a' => 'a', 'b' => 3]);
 
-        self::assertSame('3', $entity->a);
-        self::assertSame(null, $entity->b);
+        self::assertSame('a', $entity->a);
+        self::assertSame(3, $entity->b);
+
 
         $processor->updateEntity($entity, ['a' => 3, 'b' => '5']);
 
         self::assertSame('3', $entity->a);
         self::assertSame(5, $entity->b);
 
-        $processor->updateEntity($entity, ['a' => ['a' => [5, 42], 7], 'b' => '5']);
 
-        self::assertSame('3', $entity->a);
-        self::assertSame(5, $entity->b);
+        try {
+            $processor->updateEntity($entity, ['a' => ['a' => [5, 42], 7], 'b' => '5']);
+
+            self::fail('Should be invalid');
+        } catch (EntityProcessingException $exception) {
+        }
     }
 }
 
